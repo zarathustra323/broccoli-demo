24b8930a81b01ce9cb12232ee38d98399dd50e94
Jacob Bare <jacob@limit0.io>
2018-10-16 10:53:18 -0500

    Add funnel to brocfile

--- Brocfile.js
+++ Brocfile.js
@@ -1,1 +1,11 @@
-module.exports = 'app';
+const funnel = require('broccoli-funnel');
+
+const appRoot = 'app';
+
+// Copy single index.html file from root.
+const html = funnel(appRoot, {
+  files: ['index.html'],
+  annotation: 'Index file',
+});
+
+module.exports = html;
